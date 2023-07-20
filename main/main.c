e5c30bad7c36520d62d9eb5333087440220ae2db
B-Perederei <woncaru@gmail.com>
2023-07-20 12:06:55 +0300

    I don't know why, but it started working 💀💀💀

--- main/main.c
+++ main/main.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "esp_wifi.h"
+#include <sys/param.h>
+
 #include "esp_event.h"
 #include "esp_log.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include <esp_http_server.h>
-#include "esp_netif.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_mac.h"
 
-#include "lwip/err.h"
-#include "lwip/sys.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "lwip/inet.h"
+
+#include "esp_http_server.h"
+#include "dns_server.h"
 
 #define WIFI_SSID "AP-ESP32S3"
 #define WIFI_PASS "qwerty123"
+#define MAX_CONNECTIONS 4
+
+extern const char root_start[] asm("_binary_root_html_start");
+extern const char root_end[] asm("_binary_root_html_end");
 
 static const char *TAG = "wifi_softAP";
 
-// ================== WIFI SETUP AND CONNECTIONS ========================
+// ================== WIFI SOFTAP SETUP ========================
 static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
+    // Just logging info about connected/disconnected devices
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" join, AID=%d", MAC2STR(event->mac), event->aid);
@@ -33,84 +35,117 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
 }
 
 void wifi_init_softap(void) {
-
-    // Initialize Wi-Fi
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    // Create a default event loop
-    esp_netif_create_default_wifi_ap();
-
+    // Configuring SoftAP
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &wifi_event_handler,
-                                                        NULL,
-                                                        NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
                                                         
     wifi_config_t wifi_config = {
         .ap = {
             .ssid = WIFI_SSID,
             .ssid_len = strlen(WIFI_SSID),
             .password = WIFI_PASS,
-            .max_connection = 10, // Maximum number of clients connected to the AP
+            .max_connection = MAX_CONNECTIONS,
             .authmode = WIFI_AUTH_WPA_WPA2_PSK
         },
     };
+    if (strlen(WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+
+    esp_netif_ip_info_t ip_info;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ip_info);
+
+    char ip_addr[16];
+    inet_ntoa_r(ip_info.ip.addr, ip_addr, 16);
+    ESP_LOGI(TAG, "Set up softAP with IP: %s", ip_addr);
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:'%s' password:'%s'",
+             WIFI_SSID, WIFI_PASS);
 }
 
 // ================== HTTP SERVER ========================
+// Captive page handler
+static esp_err_t root_get_handler(httpd_req_t *req)
+{
+    const uint32_t root_len = root_end - root_start;
 
-esp_err_t welcome_get_handler(httpd_req_t *req) {
-    const char *html_response = (const char *)req->user_ctx;
-    httpd_resp_send(req, html_response, strlen(html_response));
+    ESP_LOGI(TAG, "Serve root");
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_send(req, root_start, root_len);
+
     return ESP_OK;
 }
 
-
-static const httpd_uri_t welcome = {
-    .uri       = "/welcome",
-    .method    = HTTP_GET,
-    .handler   = welcome_get_handler,
-    .user_ctx  = "<html><body><h1>Welcome to ESP32-S3 server</h1></body></html>"
+static const httpd_uri_t root = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = root_get_handler
 };
+
+// HTTP Error (404) Handler (Redirects all requests to the root page)
+esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
+{
+    // Set status
+    httpd_resp_set_status(req, "302 Temporary Redirect");
+    // Redirect to the "/" root directory
+    httpd_resp_set_hdr(req, "Location", "/");
+    // iOS requires content in the response to detect a captive portal, simply redirecting is not sufficient.
+    httpd_resp_send(req, "Redirect to the captive portal", HTTPD_RESP_USE_STRLEN);
+
+    ESP_LOGI(TAG, "Redirecting to root");
+    return ESP_OK;
+}
 
 static httpd_handle_t start_http_server(void) {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     httpd_handle_t server = NULL;
-
+    config.max_open_sockets = 7;
     config.lru_purge_enable = true;
+
+    // Starting the server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
-        httpd_register_uri_handler(server, &welcome);
-        return server;
+        httpd_register_uri_handler(server, &root);
+        httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, http_404_error_handler);
     }
-
-    ESP_LOGI(TAG, "Error starting server!");
-    return NULL;
+    return server;
 }
 
 void app_main(void) {
-    // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
 
+    esp_log_level_set("httpd_uri", ESP_LOG_ERROR);
+    esp_log_level_set("httpd_txrx", ESP_LOG_ERROR);
+    esp_log_level_set("httpd_parse", ESP_LOG_ERROR);
+
+    // Initialize networking stack
+    ESP_ERROR_CHECK(esp_netif_init());
+    
+    // Create default event loop needed by the  main app
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    
+    // Initialize NVS needed by Wi-Fi
+    ESP_ERROR_CHECK(nvs_flash_init());
+    
+    // Initialize Wi-Fi including netif with default config
+    esp_netif_create_default_wifi_ap();
+
+    // Initialise ESP32 in SoftAP mode
     ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
     wifi_init_softap();
     ESP_LOGI(TAG, "AP is up");
 
-    // Start the server
-    static httpd_handle_t server = NULL;
-    server = start_http_server();
-    ESP_LOGI(TAG, "HTTP Server is up");
+    // Start the server for the first time
+    start_http_server();
+    ESP_LOGI(TAG, "HTTP server is up");
+
+    // Start the DNS server that will redirect all queries to the softAP IP
+    start_dns_server();
+    ESP_LOGI(TAG, "DNS server is up");
 }
